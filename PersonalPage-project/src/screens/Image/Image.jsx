56187a2b461350445f462f0cf876e3ee2d85d74e
Tobias G. Soares <88210620+TobiasGustavo@users.noreply.github.com>
2023-10-17 11:31:41 -0300

    project

---
+++ PersonalPage-project/src/screens/Image/Image.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export const Image = () => {
+  return (
+    <div className="image">
+      <img
+        className="prototipofigmatobias"
+        alt="Prototipofigmatobias"
+        src="https://c.animaapp.com/dbxw8wYB/img/prototipofigmatobias-1.png"
+      />
+    </div>
+  );
+};
